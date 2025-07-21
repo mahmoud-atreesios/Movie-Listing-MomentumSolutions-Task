7ff46711a0b09980dde8702d2e1a89065ee6c941
mahmoud-atreesios <136765078+mahmoud-atreesios@users.noreply.github.com>
2025-07-21 16:35:31 +0300

    Intial Commit

---
+++ MovieListing-MomentumSolutions-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListing-MomentumSolutions-Task
+//
+//  Created by Mahmoud Mohamed Atrees on 21/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
